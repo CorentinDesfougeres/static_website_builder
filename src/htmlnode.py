ed0b25ac5d43ccc39375671f99be53984dff7b4e
CorentinDesfougeres <corentin.desfougeres@gmail.com>
2026-03-06 15:36:30 +0100

    Defined html LeafNode and their to_html()

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -8,10 +8,10 @@ class HTMLNode:
         self. children = children
         self.props = props
     
-    def to_html(self):
+    def to_html(self) -> str:
         raise NotImplementedError
 
-    def props_to_html(self):
+    def props_to_html(self) -> str:
         if not self.props:
             return ""
         result = ""
@@ -20,5 +20,23 @@ class HTMLNode:
         return result.strip()
     
     def __repr__(self):
-        return f"HTML Node: {self.tag}, {self.value}"
+        return f"HTML Node: {self.tag}, {self.value}, {self.children}, {self.props}"
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag :str|None, value :str|None, props :dict|None =None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self) -> str:
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value")
+        
+        if not self.tag:
+            return(self.value)
+        
+        if not self.props:
+            return f'<{self.tag}>{self.value}</{self.tag}>'
+        
+        return f'<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+        return f"HTML Leaf Node: {self.tag}, {self.value}, {self.props}"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,21 +1,51 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    # Testing props_to_html
     def test_props_to_html_empty(self):
-        node = HTMLNode("<a>", "A link", [], None)
+        node = HTMLNode("p", "Some text.", [], None)
         expected_string = ''
         self.assertEqual(node.props_to_html(), expected_string)
     
     def test_props_to_html_one_attribute(self):
-        node = HTMLNode("<a>", "A link", [], {"href": "https://www.google.com"})
+        node = HTMLNode("a", "A link", [], {"href": "https://www.google.com"})
         expected_string = 'href="https://www.google.com"'
         self.assertEqual(node.props_to_html(), expected_string)
     
     def test_props_to_html_two_attributes(self):
-        node = HTMLNode("<a>", "A link", [], {"href": "https://www.google.com", "target": "_blank"})
+        node = HTMLNode("a", "A link", [], {"href": "https://www.google.com", "target": "_blank"})
         expected_string = 'href="https://www.google.com" target="_blank"'
         self.assertEqual(node.props_to_html(), expected_string)
+    
+    def test_leaf_to_html_no_props(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        expected_string = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(node.to_html(), expected_string)
+    
+    def test_leaf_to_html_one_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        expected_string = '<a href="https://www.google.com">Click me!</a>'
+        self.assertEqual(node.to_html(), expected_string)
+    
+    def test_leaf_to_html_two_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com", "target": "_blank"})
+        expected_string = '<a href="https://www.google.com" target="_blank">Click me!</a>'
+        self.assertEqual(node.to_html(), expected_string)
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Raw string.")
+        expected_string = 'Raw string.'
+        self.assertEqual(node.to_html(), expected_string)
+    
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        expected_string = "<p>This is a paragraph of text.</p>"
+        result = ""
+        try :
+            result = node.to_html()
+        except ValueError as e:
+            result = e
+        self.assertEqual(result, expected_string)
+
